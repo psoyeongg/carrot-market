97a917dec3a0a4c596f48cc459ec07b78fe915e7
psoyeongg <psoyeongg@gmail.com>
2024-06-12 13:36:37 +0900

    Plugins

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <main className="flex items-center justify-center w-full h-screen p-5 bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100">
       <div className="flex flex-col w-full max-w-screen-sm gap-4 p-5 bg-white shadow-lg rounded-3xl">
         <a href="#">aaaa</a>
+        <input type="text" />
         <button className="btn">Submit</button>
       </div>
     </main>
