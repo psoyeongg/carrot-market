355e79ba25cee8eba558f77c1632a346f88d2204
psoyeongg <psoyeongg@gmail.com>
2024-06-12 13:23:33 +0900

    Directives

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ export default function Home() {
   return (
     <main className="flex items-center justify-center w-full h-screen p-5 bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100">
       <div className="flex flex-col w-full max-w-screen-sm gap-4 p-5 bg-white shadow-lg rounded-3xl">
-        <button className="w-full h-10 text-white bg-black rounded-sexy-name me-tomato">Submit</button>
+        <a href="#">aaaa</a>
+        <button className="btn">Submit</button>
       </div>
     </main>
   );
 }
+ 
